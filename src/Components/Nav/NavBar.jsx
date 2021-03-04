b9cbf4cdfcaae1bae91789716b3d575fc261fce9
Jitul Teron <jitulteron9@gmail.com>
2021-03-04 10:35:22 +0530

    fixed mobile view

--- src/Components/Nav/NavBar.jsx
+++ src/Components/Nav/NavBar.jsx
@@ -8,8 +8,8 @@ const Nav =()=>{
 <div class="nav-wrapper">
   <ul id="nav-mobile" class="left hide-on-med-and-down">
         <li className="logo_align"><a href="#" class=" logo_pic"><img src={logo} /></a></li>      
-        <li><Link to="/">Multiple Certificate</Link></li>
-        <li><Link to="/page3">One Certificate</Link></li>
+        {/* <li><Link to="/">Multiple Certificate</Link></li>
+        <li><Link to="/page3">One Certificate</Link></li> */}
   </ul>
   <a href="#" class="brand-logo upper_logo center">Technology Academy</a>
   <ul id="nav-mobile" class="right hide-on-med-and-down">
